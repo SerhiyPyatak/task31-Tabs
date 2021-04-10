08a826e2e427dcc8861d029edd27404d5dfac05e
unknown <C:\The Bat!>
2021-04-10 17:33:55 +0300

    This commit initializes project and adds page background and begins tabs creating

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+$(document).ready(function(){
+ 
+    // SmartTab initialize
+    $('#smarttab').smartTab();
+   
+  });
+/*
+$(() => {$( "#tabs" ).tabs(
+    {
+        classes: {
+            "tab-content": "tabblock"
+        }
+    }
+);});*/
