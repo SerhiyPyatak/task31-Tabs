8b1fe04460a6d43ce8d7174cb10844231a08d10b
SerhiyPyatak <serhiypyatak@yahoo.com>
2021-04-11 15:06:52 +0300

    This commit almost finishes info tab (except circle progress)

--- js/script.js
+++ js/script.js
@@ -9,30 +9,17 @@ $(document).ready(function(){
         const tabAnchor = anchorObject[0];
         const tabIcon = $(tabAnchor).find("> div")[0];
         if ($(tabIcon).hasClass("sprite__info-icon")) {
-            console.log('Info tab selected!');
             $(tabIcon).css("background-position", "-5px -0px");
             $(".sprite__ratings-icon").css("background-position", "-5px -150px");
             $(".sprite__buy-icon").css("background-position", "-5px -79px");
         } else if ($(tabIcon).hasClass("sprite__ratings-icon")){
-            console.log('Ratings tab selected!');
             $(tabIcon).css("background-position", "-5px -176px");
             $(".sprite__info-icon").css("background-position", "-5px -27px");
             $(".sprite__buy-icon").css("background-position", "-5px -79px");
         } else if ($(tabIcon).hasClass("sprite__buy-icon")) {
-            console.log('Buy tab selected!');
             $(tabIcon).css("background-position", "-5px -54px");
             $(".sprite__info-icon").css("background-position", "-5px -27px");
             $(".sprite__ratings-icon").css("background-position", "-5px -150px");
         };        
     });
   });
-
-
-/*
-$(() => {$( "#tabs" ).tabs(
-    {
-        classes: {
-            "tab-content": "tabblock"
-        }
-    }
-);});*/
